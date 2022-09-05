bae0168ad82da488882dc922e3be4e35b48fd115
Uyin Solomon <rootech1@hotmail.com>
2022-09-05 15:35:29 +0000

    xxx

--- community/models.py
+++ community/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
 from admin_panel.models import *
+# todo import admin panel models
 
 # Create your models here.
 from django.db.models import Count
--- ocean/settings.py
+++ ocean/settings.py
@@ -90,9 +90,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ocean',
-        'USER': 'anton',
-        'PASSWORD': '258963',
-        'HOST': '192.168.1.6',
+        'USER': 'root',
+        'PASSWORD': 'Sunderland@411',
+        'HOST': 'localhost',
         'PORT': '3306'
     }
 }
