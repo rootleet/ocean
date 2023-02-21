9a8de6a4ce76e5dbde8af64312dd70175a89bacb
Uyin Solomon <34033122+rootleet@users.noreply.github.com>
2023-02-21 10:23:56 +0000

    x place

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -486,25 +486,25 @@ class SmsApi(models.Model):
 
 
 # sms queued
-class Sms(models.Model):
-    api = models.ForeignKey('SmsApi', on_delete=models.CASCADE)
-    to = models.CharField(max_length=20, null=False)
-    message = models.TextField(null=False)
-
-    last_tried_date = models.TextField()
-    last_tried_time = models.TextField()
-
-    created_date = models.DateField(auto_now_add=True)
-    created_time = models.TimeField(auto_now=True)
-    status = models.IntegerField(default=0)
-
-
-# log sms responses
-class SmsResponse(models.Model):
-    sms = models.ForeignKey(Sms, on_delete=models.CASCADE)
-    resp_code = models.TextField()
-    resp_msg = models.TextField()
-
-    created_date = models.DateField(auto_now_add=True)
-    created_time = models.TimeField(auto_now=True)
-    status = models.IntegerField(default=0)
+# class Sms(models.Model):
+#     api = models.ForeignKey('SmsApi', on_delete=models.CASCADE)
+#     to = models.CharField(max_length=20, null=False)
+#     message = models.TextField(null=False)
+#
+#     last_tried_date = models.TextField()
+#     last_tried_time = models.TextField()
+#
+#     created_date = models.DateField(auto_now_add=True)
+#     created_time = models.TimeField(auto_now=True)
+#     status = models.IntegerField(default=0)
+#
+#
+# # log sms responses
+# class SmsResponse(models.Model):
+#     sms = models.ForeignKey(Sms, on_delete=models.CASCADE)
+#     resp_code = models.TextField()
+#     resp_msg = models.TextField()
+#
+#     created_date = models.DateField(auto_now_add=True)
+#     created_time = models.TimeField(auto_now=True)
+#     status = models.IntegerField(default=0)
