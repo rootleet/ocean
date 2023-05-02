8dc8dd04bbba5cc795feb9d76e63f2b4b7c7cbd4
Uyin Solomon <34033122+rootleet@users.noreply.github.com>
2023-05-02 15:28:36 +0000

    GRACE

--- ocean/settings.py
+++ ocean/settings.py
@@ -144,7 +144,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/repositories/ocean/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
