3f99dec630c60798c5f60eb87f218dca50f1f9a5
Uyin Solomon <34033122+rootleet@users.noreply.github.com>
2023-06-13 07:12:42 +0000

    API Excell export done

--- apiv2/views.py
+++ apiv2/views.py
@@ -12,6 +12,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from admin_panel.models import TicketHd, SmsApi, Sms, TaskHD, TaskTrans
 from appscenter.models import AppsGroup, App, AppAssign, VersionControl
+from community.models import tags
 from inventory.models import Computer
 
 
@@ -370,11 +371,13 @@ def api_function(request):
                     response['message'] = arr
 
                 if module == 'taskmanager':
+                    import re
+                    clean = re.compile('<.*?>')
+
                     task = data.get('task')
                     file_format = data.get('format')
                     domain = data.get('domain')
                     status = data.get('status', 0)
-
 
                     if file_format == 'excel':
                         import openpyxl
@@ -388,58 +391,68 @@ def api_function(request):
                         sheet['D1'] = 'LAST TRANSACTION DATE'
                         sheet['E1'] = 'LAST TRANSACTION'
 
-
                     if domain == '*':
                         task_hd = TaskHD.objects.filter(status=status)
                     else:
                         task_hd = TaskHD.objects.filter(status=status, domain_id=domain)
                     arr = []
-                    for hd in task_hd:
-                        entry_uni = hd.entry_uni
-                        type = hd.type
-                        ref = hd.ref
-                        title = hd.title
-                        description = hd.description
-                        added_on = hd.added_on
-                        edited_on = hd.edited_on
-                        domain = hd.domain.tag_dec
+                    if task_hd.count() > 0:
+                        for hd in task_hd:
+                            entry_uni = hd.entry_uni
+                            type = hd.type
+                            ref = hd.ref
+                            title = hd.title
+                            description = f"{re.sub(clean,'',hd.description)}"
+                            added_on = hd.added_on
+                            edited_on = hd.edited_on
+                            domain = hd.domain.tag_dec
 
-                        trans_filter = TaskTrans.objects.filter(entry_uni=entry_uni)
+                            trans_filter = TaskTrans.objects.filter(entry_uni=entry_uni)
 
-                        if file_format == 'json':
-                            trans = []
-                            for tran in trans_filter:
-                                trans.append({'tran_title': tran.tran_title, 'tran_descr': tran.tran_descr})
-                            arr.append({
-                                'uni': entry_uni, 'type': type, 'ref': ref, 'title': title,
-                                'desc': description, 'add_date': added_on, 'edit_date': edited_on,
-                                'domain': domain, 'trans': trans
-                            })
+                            if file_format == 'json':
+                                trans = []
+                                for tran in trans_filter:
+                                    trans.append({'tran_title': tran.tran_title, 'tran_descr': tran.tran_descr})
+                                arr.append({
+                                    'uni': entry_uni, 'type': type, 'ref': ref, 'title': title,
+                                    'desc': description, 'add_date': added_on, 'edit_date': edited_on,
+                                    'domain': domain, 'trans': trans
+                                })
 
-                            response['message'] = arr
+                                response['message'] = arr
 
-                        elif file_format == 'excel':
-                            row = 2
-                            for task in task_hd:
-                                tran_time = 'NONE'
-                                tran_desc = 'NONE'
-                                trans_count = TaskTrans.objects.filter(entry_uni=task.entry_uni)
-                                if trans_count.count() > 0:
-                                    tran = trans_count.last()
-                                    tran_time = str(tran.created_on)
-                                    tran_desc = tran.tran_descr
+                            elif file_format == 'excel':
+                                row = 2
+                                for task in task_hd:
+                                    tran_time = 'NONE'
+                                    tran_desc = 'NONE'
+                                    trans_count = TaskTrans.objects.filter(entry_uni=task.entry_uni)
+                                    if trans_count.count() > 0:
+                                        tran = trans_count.last()
+                                        tran_time = str(tran.created_on)
+                                        tran_desc = tran.tran_descr
 
+                                    sheet[f'A{row}'] = task.domain.tag_dec
+                                    sheet[f'B{row}'] = task.title
+                                    sheet[f'C{row}'] = f"{re.sub(clean,'',task.description)}"
+                                    sheet[f'D{row}'] = tran_time
+                                    sheet[f'E{row}'] = tran_desc
+                                    row += 1
 
-                                sheet[f'A{row}'] = task.domain.tag_dec
-                                sheet[f'B{row}'] = task.title
-                                sheet[f'C{row}'] = task.description
-                                sheet[f'D{row}'] = tran_time
-                                sheet[f'E{row}'] = tran_desc
-                                row+=1
+                                file_name = f"static/general/docs/issues.xlsx"
+                                workbook.save(file_name)
+                                response['message'] = file_name
+                    else:
+                        response['status_code'] = 404
+                        response['message'] = "No Data To Retrieve"
+                if module == 'domain':
+                    arr = []
+                    domains = tags.objects.all()
 
-                            file_name = f"static/general/docs/issues.xlsx"
-                            workbook.save(file_name)
-                            response['message'] = file_name
+                    for domain in domains:
+                        arr.append({'pk': domain.pk, 'desc': domain.tag_dec})
+
+                    response['message'] = arr
 
             except KeyError as e:
                 response["status_code"] = 400
--- static/general/js/classes/issues.js
+++ static/general/js/classes/issues.js
@@ -71,6 +71,99 @@ class Issues {
 
         }
     }
+
+
+
+    Export(){
+        let d_opt = ''
+        let domains = api.view({"module":"domain"})['message']
+
+        for (let d = 0; d < domains.length ; d++) {
+            let op = domains[d]
+            d_opt += `<option value="${op['pk']}">${op['desc']}</option>`
+        }
+        Swal.fire({
+            title: 'Export Data',
+            html:
+              '<form style="text-align: left !important">' +
+              '<div class="form-group mb-3">' +
+              '<label for="doc_type">Document Type</label>' +
+              '<select id="doc_type" class="form-control">' +
+              '<option value="" selected disabled>Select Document</option>' +
+              '<option value="excel">Excel</option>' +
+              '<option value="json" disabled>JSON</option>' +
+              '</select>' +
+              '</div>' +
+              '<div class="form-group mb-3">' +
+              '<label for="domain">Domain</label>' +
+              '<select id="domain" class="form-control">' +
+              '<option value="" selected disabled>Select Domain</option>' +
+              d_opt +
+              '</select>' +
+              '</div>' +
+              '<div class="form-group ">' +
+              '<label for="status">Status</label>' +
+              '<select id="status" class="form-control">' +
+              '<option value="" selected disabled>Select Status</option>' +
+              '<option value="1">Close</option>' +
+              '<option value="0">Open</option>' +
+              '<option value="*">All</option>' +
+              '</select>' +
+              '</div>' +
+              '</form>',
+            focusConfirm: false,
+            showCancelButton: true,
+            confirmButtonText: 'Export',
+            cancelButtonText: 'Close',
+            preConfirm: function() {
+              var docType = $('#doc_type').val();
+              var domain = $('#domain').val();
+              var status = $('#status').val();
+
+              if (!docType || !domain || !status) {
+                Swal.showValidationMessage('Please select an option for all fields');
+                return false;
+              }
+
+              return { docType: docType, domain: domain, status: status };
+            }
+          }).then(function(result) {
+            if (result.isConfirmed) {
+              var docType = result.value.docType;
+              var domain = result.value.domain;
+              var status = result.value.status;
+              console.log('Doc Type:', docType);
+              console.log('Domain:', domain);
+              console.log('Status:', status);
+
+              let data = {
+                  'module':'taskmanager',
+                  'data':{
+                      'format':docType,'domain':domain,'status':status
+                  }
+              }
+
+              console.table(data)
+
+              let view = api.view(data)
+                console.table(view)
+                if(view['status_code'] === 200){
+                    Swal.fire({
+                        'icon':'success',
+                        'text':"Download file with the link below",
+                        'html':`Download file with the link below <br><a href="${view['message']}" target="_blank">LINK</a>`
+                    })
+                } else {
+                    Swal.fire({
+                        'icon':'info',
+                        'title':view['status_code'],
+                        'text':view['message'],
+                    })
+                }
+            }
+          });
+    }
+
 }
 
 const issues = new Issues();
