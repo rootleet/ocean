233f212a198611cd30e1ff8a56be244e421ce18c
Uyin Solomon <34033122+rootleet@users.noreply.github.com>
2023-08-22 09:58:40 +0000

    Add ReportLegend, LegendSubs, ReportForms models
    Introduced three new models to the reports app: ReportLegend, LegendSubs and ReportForms. ReportLegend and LegendSubs will allow users to create and manage various report categories and their corresponding subcategories.  ReportForms model has been added to enable users in crafting custom report templates. This enhancement aims to streamline the report generation process by providing more structural flexibility and user customization.

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class ReportLegend(models.Model):
+    name = models.CharField(unique=True, null=False, blank=False, max_length=200)
+    description = models.TextField()
+
+    created_date = models.DateField(auto_now_add=True)
+    created_time = models.TimeField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+    updated_time = models.TimeField(auto_now=True)
+
+    status = models.IntegerField(default=1)  # 1 active, 0 not, 99 done
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def subs(self):
+        return LegendSubs.objects.filter(legend=self)
+
+
+class LegendSubs(models.Model):
+    name = models.CharField(unique=True, null=False, blank=False, max_length=200)
+    action = models.CharField(unique=True, null=False, blank=False, max_length=200)
+    description = models.TextField()
+
+    legend = models.ForeignKey(ReportLegend, on_delete=models.SET_NULL, blank=True, null=True)
+
+    created_date = models.DateField(auto_now_add=True)
+    created_time = models.TimeField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+    updated_time = models.TimeField(auto_now=True)
+
+
+
+    # status = models.IntegerField(default=1)  # 1 active, 0 not, 99 done
+    # owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+
+
+class ReportForms(models.Model):
+    key = models.CharField(unique=True, null=False, blank=False, max_length=200)
+    description = models.TextField()
+    code = models.TextField()
+
+    created_date = models.DateField(auto_now_add=True)
+    created_time = models.TimeField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+    updated_time = models.TimeField(auto_now=True)
+
+    status = models.IntegerField(default=1)  # 1 active, 0 not, 99 done
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
