ff0b2be2e5b00c52773a30f7d4e644383c14d897
Uyin Solomon <34033122+rootleet@users.noreply.github.com>
2023-10-01 07:57:53 +0000

    cromjob for generation reports

---
+++ cronjobs/generate_department_reports.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+curl --location --request VIEW 'http://127.0.0.1:8000/adapi/' \
+--header 'Content-Type: application/json' \
+--header 'Cookie: csrftoken=etMJ2XQLTdShoVg4UxIfhE67JjVuYtNP' \
+--data '{
+    "module":"dept_report"
+}'
