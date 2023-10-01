763390ce8bd6f59f13c18e117eca82ba74879866
Uyin Solomon <34033122+rootleet@users.noreply.github.com>
2023-10-01 08:02:07 +0000

    Send Department Emails

--- cronjobs/send_department_emails.sh
+++ cronjobs/send_department_emails.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
-curl --location --request VIEW 'http://127.0.0.1/adapi/' \
+curl --location --request VIEW 'http://127.0.0.1/reports/api/' \
 --header 'Content-Type: application/json' \
 --header 'Cookie: csrftoken=etMJ2XQLTdShoVg4UxIfhE67JjVuYtNP' \
 --data '{
-    "module":"dept_report"
+   "data":{
+       "doc":"send_dept_mail"
+   }
 }'
