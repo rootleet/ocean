f07f49ad6e65d355e03ddd04a238db9400e20483
Uyin Solomon <34033122+rootleet@users.noreply.github.com>
2023-10-30 14:33:03 +0000

    db interface updated

--- retail/db.py
+++ retail/db.py
@@ -15,21 +15,9 @@ def ret_cursor(host=RET_DB_HOST, port=RET_DB_PORT, db=RET_DB_NAME, user=RET_DB_U
 
 
 def get_stock(item_code):
-    server = RET_DB_HOST
-    database = RET_DB_NAME
-    username = RET_DB_USER
-    password = RET_DB_PASS
 
-    # Establish a connection to the database
-    stock_connection = pyodbc.connect(
-        'DRIVER={SQL Server};'
-        'SERVER=' + server + ';'
-                             'DATABASE=' + database + ';'
-                                                      'UID=' + username + ';'
-                                                                          'PWD=' + password + ';'
-    )
 
-    stock_cursor = stock_connection.cursor()
+    stock_cursor = ret_cursor()
     query = f"SELECT stock.loc_id, (select br_name from branch where br_code = stock.loc_id ) as loc_name , isnull(sum(qty),0) as qty, ('2') as trtype, stock_price.avg_cost, stock_price.last_net_cost, stock_price.last_rec_supp, stock_price.last_rec_price, stock_price.local_supp_curr, stock_price.last_rec_date, stock_price.last_cost2, stock_price.last_cost3, isnull(sum(stock.item_wt),0) as tot_wt, stock_price.last_rec_um FROM stock LEFT OUTER JOIN stock_price ON stock.item_code = stock_price.item_code AND stock.loc_id = stock_price.loc_id, user_loc_access ,prod_mast WHERE ( user_loc_access.loc_id = stock.loc_id ) and ( stock.item_code = prod_mast.item_code ) and( ( stock.item_code = '{item_code}' ) AND ( user_loc_access.loc_access = '1' ) AND ( user_loc_access.user_id = '411' ) AND( prod_mast.item_type in ('1','3','5','7')) ) GROUP BY stock.loc_id, stock_price.avg_cost, stock_price.last_net_cost, stock_price.last_rec_price, stock_price.last_rec_um, stock_price.local_supp_curr, stock_price.last_rec_supp, stock_price.last_rec_date, stock_price.last_cost2, stock_price.last_cost3 UNION SELECT stock_chk.loc_id, (select br_name from branch where br_code = stock_chk.loc_id ) as loc_name , isnull(sum(qty),0) as qty, '3', stock_price.avg_cost, stock_price.last_net_cost, stock_price.last_rec_supp, stock_price.last_rec_price, \
     stock_price.local_supp_curr, stock_price.last_rec_date, stock_price.last_cost2,stock_price.last_cost3, isnull(sum(stock_chk.item_wt),0) as tot_wt, stock_price.last_rec_um FROM stock_chk LEFT OUTER JOIN stock_price ON stock_chk.item_code = stock_price.item_code AND stock_chk.loc_id = stock_price.loc_id ,user_loc_access ,prod_mast \
     WHERE  ( user_loc_access.loc_id = stock_chk.loc_id ) and ( stock_chk.item_code = prod_mast.item_code ) and ( ( stock_chk.item_code = '{item_code}' ) AND ( user_loc_access.loc_access = '1' ) AND ( user_loc_access.user_id = '411' ) AND( prod_mast.item_type in ('1','3','5','7')))  GROUP BY stock_chk.loc_id, stock_price.avg_cost, stock_price.last_net_cost, stock_price.last_rec_supp, stock_price.last_rec_price, stock_price.last_rec_um, stock_price.local_supp_curr, stock_price.last_rec_date, stock_price.last_cost2, stock_price.last_cost3 ORDER BY 1 ASC "
