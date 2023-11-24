a3beca0b81d2e4333cc28121dd2f2655f94e4c20
Uyin Solomon <34033122+rootleet@users.noreply.github.com>
2023-11-24 08:47:38 +0000

    dynacmis stock variables

--- cronjobs/retail/retail.sh
+++ cronjobs/retail/retail.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-./sync_retail_groups.sh
-./sync_retail_products.sh
-./sync_retail_sub_groups.sh
+echo "RETAILER"
+
+printf "SYNCING SUPPLIERS.... \n"
 ./sync_retail_suppliers.sh
+
+printf "SYNCING GROUPS.... \n"
+./sync_retail_groups.sh
+
+printf "SYNCING SUB GROUPS.... \n"
+./sync_retail_sub_groups.sh
+
+printf "SYNCING PRODUCTS.... \n"
+./sync_retail_products.sh
+
+printf "UPDATING STOCK.... \n"
 ./update_stock.sh
