d6065b4570123e8ae8742f096ccc9f892226fe42
Uyin Solomon <34033122+rootleet@users.noreply.github.com>
2024-05-02 21:15:01 +0000

    stock monitor interface changed

--- cronjobs/retail/stock_monitor.sh
+++ cronjobs/retail/stock_monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl --location --request VIEW 'http://127.0.0.1:8000/retail/api/' \
+curl --location --request VIEW 'http://127.0.0.1/retail/api/' \
 --header 'Content-Type: application/json' \
 --header 'Cookie: csrftoken=etMJ2XQLTdShoVg4UxIfhE67JjVuYtNP' \
 --data '{
