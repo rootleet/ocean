e5688277506e5ffb9f7556438515b304be9b84d1
Uyin Solomon <34033122+rootleet@users.noreply.github.com>
2024-05-02 17:27:36 +0000

    add stock update cron job

--- cronjobs/retail/retail.sh
+++ cronjobs/retail/retail.sh
@@ -2,19 +2,19 @@
 echo "RETAILER"
 
 printf "CHECKING STOCK MONITOR"
-./update_stock.sh
+/home/repositories/ocean/cronjobs/retail/update_stock.sh
 
 printf "SYNCING SUPPLIERS.... \n"
-./sync_retail_suppliers.sh
+/home/repositories/ocean/cronjobs/retail/sync_retail_suppliers.sh
 
 printf "SYNCING GROUPS.... \n"
-./sync_retail_groups.sh
+/home/repositories/ocean/cronjobs/retail/sync_retail_groups.sh
 
 printf "SYNCING SUB GROUPS.... \n"
-./sync_retail_sub_groups.sh
+/home/repositories/ocean/cronjobs/retail/sync_retail_sub_groups.sh
 
 printf "SYNCING PRODUCTS.... \n"
-./sync_retail_products.sh
+/home/repositories/ocean/cronjobs/retail/sync_retail_products.sh
 
 printf "UPDATING STOCK.... \n"
-./update_stock.sh
+/home/repositories/ocean/cronjobs/retail/update_stock.sh
